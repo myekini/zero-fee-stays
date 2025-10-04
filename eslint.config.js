869a0fa618f871a800fa068a1c48fd671e29da9c
Muhammad Yekini <myekini1@gmail.com>
2025-10-04 18:17:07 +0100

    Fix build issues: exclude inspiration and supabase functions from compilation

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,17 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", ".next", "node_modules", "out", "build"] },
+  {
+    ignores: [
+      "dist",
+      ".next",
+      "node_modules",
+      "out",
+      "build",
+      "inspiration",
+      "supabase/functions",
+    ],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
